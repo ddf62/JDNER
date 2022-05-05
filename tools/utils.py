a73c6ab1218f77ba8418c8e3e56c0992dd99c28a
ddf62 <zhangh136@126.com>
2022-05-05 16:58:46 +0800

    Create utils.py
    utils

---
+++ tools/utils.py
@@ -0,0 +1,76 @@
+import torch
+from torch import nn
+
+class FGM():
+    def __init__(self, model):
+        self.model = model
+        self.backup = {}
+
+    def attack(self, epsilon=0.3, emb_name='robertaembeddings.word_embeddings_layer.weight'):
+        # emb_name这个参数要换成你模型中embedding的参数名
+        for name, param in self.model.named_parameters():
+            if param.requires_grad and emb_name in name:
+            	#这里加入fgm attack来判断是否进行对抗训练了
+                self.backup[name] = param.data.clone()
+                norm = torch.norm(param.grad)
+                if norm != 0 and not torch.isnan(norm):
+                    r_at = epsilon * param.grad / norm
+                    param.data.add_(r_at)
+
+    def restore(self, emb_name='robertaembeddings.word_embeddings_layer.weight'):
+        # emb_name这个参数要换成你模型中embedding的参数名
+        for name, param in self.model.named_parameters():
+            if param.requires_grad and emb_name in name: 
+                #这里加入fgm restore判断是否恢复参数了
+                assert name in self.backup
+                param.data = self.backup[name]
+        self.backup = {} 
+
+
+class ConditionalLayerNorm(nn.Module):
+    def __init__(self,
+                 normalized_shape,
+                 cond_shape,
+                 eps=1e-12):
+        super().__init__()
+
+        self.eps = eps
+
+        self.weight = nn.Parameter(torch.Tensor(normalized_shape))
+        self.bias = nn.Parameter(torch.Tensor(normalized_shape))
+
+        self.weight_dense = nn.Linear(cond_shape, normalized_shape, bias=False)
+        self.bias_dense = nn.Linear(cond_shape, normalized_shape, bias=False)
+
+        self.reset_weight_and_bias()
+
+    def reset_weight_and_bias(self):
+        """
+        此处初始化的作用是在训练开始阶段不让 conditional layer norm 起作用
+        """
+        # nn.init.normal_(self.weight)
+        # nn.init.normal_(self.bias)
+        nn.init.ones_(self.weight)
+        nn.init.zeros_(self.bias)
+
+        nn.init.zeros_(self.weight_dense.weight)
+        nn.init.zeros_(self.bias_dense.weight)
+
+    def forward(self, inputs, cond=None):
+        assert cond is not None, 'Conditional tensor need to input when use conditional layer norm'
+        cond = torch.unsqueeze(cond, 1)  # (b, 1, h*2)
+
+        weight = self.weight_dense(cond) + self.weight  # (b, 1, h)
+        bias = self.bias_dense(cond) + self.bias  # (b, 1, h)
+
+        mean = torch.mean(inputs, dim=-1, keepdim=True)  # （b, s, 1）
+        outputs = inputs - mean  # (b, s, h)
+
+        variance = torch.mean(outputs ** 2, dim=-1, keepdim=True)
+        std = torch.sqrt(variance + self.eps)  # (b, s, 1)
+
+        outputs = outputs / std  # (b, s, h)
+
+        outputs = outputs * weight + bias
+
+        return outputs
